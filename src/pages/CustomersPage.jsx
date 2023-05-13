0a69d218aa625132409750973dde433beef9cebd
NormanWilcox <wilcoxivan@hotmal.com>
2023-05-13 08:29:05 -0700

    first commit

---
+++ src/pages/CustomersPage.jsx
@@ -0,0 +1,19 @@
+import axios from axios;
+
+const CustomerPage = () => {
+  return (
+    <>
+    <main className="row">
+      <article className="col">
+        <h2>Customers</h2>
+      </article>
+    </main>
+    <section className="row">
+      
+
+    </section>
+  </>
+  )
+}
+
+export default CustomerPage
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,35 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+  } from "react-router-dom";
+  
+  import NavBar from "../components/NavBar";
+  
+  import HomePage from "../pages/HomePage";
+  import AboutPage from "../pages/AboutPage";
+  import CustomersPage from "../pages/CustomersPage";
+  import CustomerPage from "../pages/CustomerPage";
+  
+  const AppRouter = () => {
+    return (
+      <>
+        <Router>
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/customers" element={<CustomersPage />} />
+              <Route path="/customers/:id" element={<CustomerPage />} />
+  
+              <Route path="/*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+        </Router>
+      </>
+    );
+  };
+  
+  export default AppRouter;
